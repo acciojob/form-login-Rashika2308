b6d21ae1aae9445acaa17a1f82b67e612fcaeb90
Rashika2308 <rashikavidyarthi545@gmail.com>
2025-05-16 09:09:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form by its ID
+    const form = document.getElementById('form1');
 
+    // Get the trimmed values of First Name and Last Name
+    const firstName = form.fname.value.trim();
+    const lastName = form.lname.value.trim();
+
+    // Show the full name in an alert
+    alert(firstName + " " + lastName);
+
+    // Prevent the form from submitting and reloading the page
+    return false;
 }
